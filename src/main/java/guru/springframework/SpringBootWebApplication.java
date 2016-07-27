a35ce0728aa58439dda15f42ae7a61ebe43a9a5e
venividivichi <moloduy23@gmail.com>
2016-07-27 15:20:37 +0300

    change generation

--- src/main/java/guru/springframework/SpringBootWebApplication.java
+++ src/main/java/guru/springframework/SpringBootWebApplication.java
@@ -2,12 +2,20 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-public class SpringBootWebApplication {
+public class SpringBootWebApplication extends SpringBootServletInitializer{
 
-    public static void main(String[] args)  throws Exception {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWebApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootWebApplication.class, args);
     }
+
 }
